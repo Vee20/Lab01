b64704df15693e613f1eb7a774e056e5df52e561
Veera Gudla <70934190+Vee20@users.noreply.github.com>
2020-09-16 17:12:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/FragmentList.java
@@ -0,0 +1,57 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class FragmentList extends Fragment {
+ListView LV;
+
+SearchView SV;
+ArrayAdapter<String> Adapter;
+String[] activityNames = {"AIActivity", "VRActivity"};
+
+    public FragmentList() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Toast.makeText(getContext(), "List View Fragment OnCreateView", Toast.LENGTH_SHORT).show();
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        LV = (ListView) view.findViewById(R.id.listView);
+        Adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, activityNames);
+        LV.setAdapter(Adapter);
+        LV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(position == 0){
+                    Intent AI  = new Intent(getActivity(), AIActivity.class);
+                    startActivity(AI);
+                } else if(position == 1){
+                    Intent VR = new Intent(getActivity(), VRActivity.class);
+                    startActivity(VR);
+                }
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView onResumeText;
+    TextView onStartText;
+    TextView onCreateText;
+
+    @Override
+    protected void onStart() {
+        onStartText = findViewById(R.id.onStartText);
+        onStartText.setVisibility(View.VISIBLE);
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onCreateText = findViewById(R.id.onCreateText);
+        onCreateText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onResume() {
+        onResumeText = findViewById(R.id.onResumeText);
+        onResumeText.setVisibility(View.VISIBLE);
+        super.onResume();
+    }
+
+}
